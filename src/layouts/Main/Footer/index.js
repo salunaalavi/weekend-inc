b22deddf81a183ecc57dbf64c3cc9d18e82f0750
salunaalavi <salunaalavi@gmail.com>
2022-10-08 23:14:52 +0700

    Create main layout

---
+++ src/layouts/Main/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+const Footer = (props) => (
+    <div className={styles.layout} {...props}>
+        Ini Header...
+    </div>
+);
+
+export default Footer;
---
+++ src/layouts/Main/Header/index.js
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+const Header = () => (
+    <div className={styles.layout}>
+        ini header
+    </div>
+);
+
+export default Header;
