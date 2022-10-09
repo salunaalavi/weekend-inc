ea3a67f040cfef35c615b9f86520148aee97580c
salunaalavi <salunaalavi@gmail.com>
2022-10-09 19:26:43 +0700

    Create hero component

---
+++ src/components/Hero/index.js
@@ -0,0 +1,25 @@
+import styles from './styles.module.scss';
+
+function Hero() {
+    return (
+        <div className={`relative flex ${styles.hero} justify-center text-white`}>
+            <div>
+                <h1 className={`uppercase`}>
+                    WEEKEND FROM HOME
+                </h1>
+                <h3 className={`italic font-normal`}>
+                    Stay active with a little workout.
+                </h3>
+                <div className={`relative flex ${styles.wrapper} justify-center`}>
+                    <button className={`bg-white`}>
+                        <h5 className={`font-normal text-black`}>
+                            Let's Go
+                        </h5>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
