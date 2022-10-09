4c9083d1fe9952b733586c923acfd6b819f60056
salunaalavi <salunaalavi@gmail.com>
2022-10-09 12:22:33 +0700

    Create footer component and styling header container

--- src/layouts/Main/Footer/index.js
+++ src/layouts/Main/Footer/index.js
@@ -1,9 +1,10 @@
 import styles from "./styles.module.scss";
 
 const Footer = (props) => (
-    <footer {...props}>
-        <div className={styles.footer}>
-            Ini Footer
+    <footer className={`bg-bluebonnet`} {...props}>
+        <div className={`flex ${styles.container} mx-auto justify-between items-center`}>
+            <h5 className={`text-white`}>wknd<span className={`font-normal`}>@2020</span></h5>
+            <h5 className={`${styles.version} font-light text-white border-white`}>alpha version 0.1</h5>
         </div>
     </footer>
 );
