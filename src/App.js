b418302ac71892164cad7d3264e42a313d0486f7
salunaalavi <salunaalavi@gmail.com>
2022-10-10 14:19:57 +0700

    Create testimonial and testimonial card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Hero from "./components/Hero";
 import Intro from "./components/Intro";
+import Testimonial from "./components/Testimonial";
 import styles from './App.module.scss';
 
 function App() {
     return (
         <>
-            <div className={`${styles.upper} text-center bg-cameo-pink`}>
+            <div className={` text-center bg-cameo-pink`}>
                 <Hero />
                 <Intro />
+            </div>
+            <div className={` text-center text-white bg-black`}>
+                <Testimonial />
             </div>
         </>
     );
